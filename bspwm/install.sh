952c196430902e460088cfdde85601605e6e9a97
ByAncort <diego2005cortes@gmail.com>
2022-05-25 21:06:11 -0400

    actualizacion de instalador

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -9,3 +9,14 @@ sudo apt-get update
 sudo apt install -y rofi terminator bspwm sxhkd polybar feh
 
 #clonando repositorios de configuracion
+
+cd ~
+git clone https://github.com/ByAncort/Eko-Dotfiles.git
+cd Eko-Dotfiles
+cp -r * ~/.config
+sudo rm ~/.config/install.sh
+
+cd -r ~/.config/polybar/fonts/* /usr/fonts/
+
+
+reboot
